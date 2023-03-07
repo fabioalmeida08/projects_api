072d3380e38e2299d163a7845b45b9f826871b1b
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 13:27:02 -0300

    feat: created dto for create project

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -3,9 +3,9 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateProjectDto {
   @IsNotEmpty()
   @IsString()
-  email: string;
+  name: string;
 
   @IsNotEmpty()
   @IsString()
-  password: string;
+  descricao: string;
 }
