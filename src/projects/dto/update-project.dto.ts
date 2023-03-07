b52c33784f7d78e1f9cf9eb0ce4a3a31847eafa0
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 16:07:28 -0300

    refactor: add IsOptional decorator for all fields

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateProjectDto } from './create-project.dto';
+import { IsString, IsOptional } from 'class-validator';
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {
+  @IsOptional()
+  nome: string;
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+  @IsOptional()
+  descricao?: string;
+}
