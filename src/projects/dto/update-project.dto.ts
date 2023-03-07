66fdc0c1c31a61afc282465946be3ae374fffc02
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 16:08:40 -0300

    refactor: removed unused decorator import

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateProjectDto } from './create-project.dto';
-import { IsString, IsOptional } from 'class-validator';
+import { IsOptional } from 'class-validator';
 export class UpdateProjectDto extends PartialType(CreateProjectDto) {
   @IsOptional()
   nome: string;
