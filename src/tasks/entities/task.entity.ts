10cbfb8eb2ae9bb362bf3093355f0a6b72109ddd
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 14:54:55 -0300

    feat: created task entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,11 @@
-export class Task {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+import { TasksStatusEnum } from '../dto/tasks-enum';
+
+export class Task {
+  descricao: string;
+
+  data_limite: Date;
+
+  status: TasksStatusEnum;
+}
