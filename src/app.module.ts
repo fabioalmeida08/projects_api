6019f718b073217dd00baf25dd74d78384ff54b9
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 13:16:17 -0300

    chore: add projects module to the list of imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from '../db/data-source';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options)],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options), ProjectsModule],
   controllers: [],
   providers: [],
 })
