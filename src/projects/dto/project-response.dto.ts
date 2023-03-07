225192b05b452803da125377413e0d52e143192c
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 20:52:46 -0300

    refactor: refactored Project Dtos

--- src/projects/dto/project-response.dto.ts
+++ src/projects/dto/project-response.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { ProjectDto } from './project-body.dto';
+import { CreateProjectDto } from './project-create.dto';
 
-export class ProjectResponseDto extends ProjectDto {
+export class ProjectResponseDto extends CreateProjectDto {
   @ApiProperty({ example: '9af8d658-9391-4cd1-a9a3-149e33b384b6' })
   id: string;
 }
--- src/projects/dto/project-update.dto.ts
+++ src/projects/dto/project-update.dto.ts
@@ -1,13 +1,13 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateProjectDto } from './create-project.dto';
+import { CreateProjectDto } from './project-create.dto';
 import { IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 export class UpdateProjectDto extends PartialType(CreateProjectDto) {
   @IsOptional()
-  @ApiProperty({required:false, example: "updated project name"})
+  @ApiProperty({ required: false, example: 'updated project name' })
   nome: string;
 
   @IsOptional()
-  @ApiProperty({required: false, example: "updated project description"})
+  @ApiProperty({ required: false, example: 'updated project description' })
   descricao?: string;
 }
