ff80365df39167bd5ecb33d2a155b61de7d49271
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 15:14:27 -0300

    feat: created task entity using typeorm

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -2,10 +2,17 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 import { TasksStatusEnum } from '../dto/tasks-enum';
 
+@Entity()
 export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
   descricao: string;
 
+  @Column()
   data_limite: Date;
 
+  @Column()
   status: TasksStatusEnum;
 }
