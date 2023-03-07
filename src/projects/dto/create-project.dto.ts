fbc8121ec7a197057fefba65559e5be2837d1079
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 14:11:42 -0300

    fix: fixed typo on field "nome" in createProjectDto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateProjectDto {
   @IsNotEmpty()
   @IsString()
-  name: string;
+  nome: string;
 
   @IsNotEmpty()
   @IsString()
