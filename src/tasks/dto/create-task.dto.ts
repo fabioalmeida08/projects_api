1f5aab51c77f76bf935289738d7409a0d3a7435f
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 15:02:41 -0300

    feat: created Dto for create task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTaskDto {}
+import { IsNotEmpty, IsString, IsEnum, IsDate } from 'class-validator';
+import { TasksStatusEnum } from './tasks-enum';
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  descricao: string;
+
+  @IsDate()
+  data_limite: Date;
+
+  @IsEnum(TasksStatusEnum)
+  @IsNotEmpty()
+  status: TasksStatusEnum;
+}
