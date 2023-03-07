f7136bcff62de12ce6513c3c37c6762db69f1aff
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 16:10:46 -0300

    feat: add IsOptional decorator for all fields

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateTaskDto } from './create-task.dto';
+import { IsOptional } from 'class-validator';
+import { TasksStatusEnum } from './tasks-enum';
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsOptional()
+  descricao?: string;
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+  @IsOptional()
+  data_limite?: Date;
+
+  @IsOptional()
+  status?: TasksStatusEnum;
+}
