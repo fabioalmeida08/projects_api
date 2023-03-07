753f2cf5a4e13b68228485221d10390ae8a548e0
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 14:52:44 -0300

    feat: generated tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from '../db/data-source';
 import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options), ProjectsModule],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options), ProjectsModule, TasksModule],
   controllers: [],
   providers: [],
 })
