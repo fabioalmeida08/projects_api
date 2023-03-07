04612561c1827fcfd9db180372d73c0cb558ea79
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 12:34:50 -0300

    refactor: removed default service and controller created by nestcli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from '../db/data-source';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(AppDataSource.options)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
