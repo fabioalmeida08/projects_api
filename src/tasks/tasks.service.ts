8447b78f5963e80431ab3db31b2894e82d5a1af5
Fábio <fabioalmeida0896@gmail.com>
2023-03-07 21:01:23 -0300

    refactor: changed imports to the refactored version of dtos filenames

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { CreateTaskDto } from './dto/task-create.dto';
+import { UpdateTaskDto } from './dto/task-update.dto';
 import { Task } from './entities/task.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
